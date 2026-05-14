cfc570b055d05f4227b926c68db15e203c613a50
pmadrigalv <madrigalpedro1@outlook.es>
2026-05-13 22:46:39 -0600

    Make MySQL auth fix idempotent

--- docker/mysql/fix-existing-volume-auth.sql
+++ docker/mysql/fix-existing-volume-auth.sql
@@ -1,3 +1,5 @@
+CREATE USER IF NOT EXISTS 'app'@'%' IDENTIFIED BY 'secret';
 ALTER USER 'app'@'%' IDENTIFIED WITH mysql_native_password BY 'secret';
+CREATE USER IF NOT EXISTS 'app'@'localhost' IDENTIFIED BY 'secret';
 ALTER USER 'app'@'localhost' IDENTIFIED WITH mysql_native_password BY 'secret';
 FLUSH PRIVILEGES;
